01d019769a3bd2a74e1c87df3db0c1801438d204
MrDoffing <nathanael.remstedt@outlook.com>
2021-03-13 00:43:12 +0100

    added stuff

--- PA2552-SoftwareTesting-Project/PA2552-SoftwareTesting-Project.cpp
+++ PA2552-SoftwareTesting-Project/PA2552-SoftwareTesting-Project.cpp
@@ -9,18 +9,29 @@ namespace PA2552SoftwareTestingProject
 	{
 	public:
 		
+		TEST_CLASS_INITIALIZE(ClassInitialize)
+		{
+			Logger::WriteMessage("In Class Initialize");
+		}
+		TEST_CLASS_CLEANUP(ClassCleanup)
+		{
+			Logger::WriteMessage("In Class Cleanup");
+		}
 		TEST_METHOD(TestMethod_Add)
 		{
+			Logger::WriteMessage("InMethod - Add");
 			Assert::IsTrue(Calculator::add(3, 4) == 7);
 			Assert::IsTrue(Calculator::add(-2, -1) == -3);
 		}
 		TEST_METHOD(TestMethod_Sub)
 		{
+			Logger::WriteMessage("InMethod - Sub");
 			Assert::IsTrue(Calculator::sub(3, 4) == -1);
 			Assert::IsTrue(Calculator::sub(5, -1) == 6);
 		}
 		TEST_METHOD(TestMethod_Mul)
 		{
+			Logger::WriteMessage("InMethod - Mul");
 			Assert::IsTrue(Calculator::mul(4, 5) == 20);
 		}
 	};
